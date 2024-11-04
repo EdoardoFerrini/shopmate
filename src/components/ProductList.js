02e4df058ef7c5bb14c5d3c423aa50144f459bac
EdoardoFerrini <edoardo.ferrini@gmail.com>
2024-11-04 21:53:37 +0100

    add filter logic and useCallback

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,33 +1,46 @@
 import "./ProductList.css";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback} from "react";
 
 export const ProductList = () => {
-
   const [products, setProducts] = useState([]);
   const [url, setUrl] = useState("http://localhost:8000/products");
-  const [counter, setCounter] = useState(0)
+
+  const fetchProducts = useCallback(async () => {
+    const response = await fetch(url);
+    const data = await response.json();
+    setProducts(data);
+  }, [url])
 
   useEffect(() => {
-    fetch(url)
-      .then((response) => response.json())
-      .then((data) => setProducts(data));
-  }, [url]);
+    fetchProducts();
+  }, [fetchProducts]);
 
   return (
     <section>
-        <div className="filter">
-            <button className="available" onClick={()=> setUrl("http://localhost:8000/products")}>All</button>
-            <button onClick={()=> setUrl("http://localhost:8000/products?available=true")}>In Stock</button>
-        </div>
+      <div className="filter">
+        <button
+          className="available"
+          onClick={() => setUrl("http://localhost:8000/products")}
+        >
+          All
+        </button>
+        <button
+          onClick={() =>
+            setUrl("http://localhost:8000/products?available=true")
+          }
+        >
+          In Stock
+        </button>
+      </div>
 
       {products.map((product) => (
-        <div className="card">
+        <div key={product.id} className="card">
           <p className="id">{product.id}</p>
           <p className="name">{product.name}</p>
           <p className="info">
             <span>€{product.price}</span>
             <span className={product.available ? "available" : "unavailable"}>
-              {product.available}
+              {product.available ? "In Stock" : "not in stock"}
             </span>
           </p>
         </div>
