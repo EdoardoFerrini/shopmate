21c363577018d56c87e6bcf126556a1f5d60b2bc
EdoardoFerrini <edoardo.ferrini@gmail.com>
2024-11-05 17:08:16 +0100

    added body parameter with useRef

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,9 +1,10 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 
-export const useFetch = (url) => {
+export const useFetch = (url, _body) => {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState();
+  const body = useRef(_body);
 
   useEffect(() => {
     const controller = new AbortController();
@@ -11,7 +12,7 @@ export const useFetch = (url) => {
     const fetchData = async () => {
       setLoading(true);
       try {
-        const response = await fetch(url, {signal: controller.signal});
+        const response = await fetch(url, { signal: controller.signal });
         if (!response.ok) {
           throw new Error(response.statusText);
         }
@@ -26,6 +27,6 @@ export const useFetch = (url) => {
     };
     fetchData();
     return () => controller.abort();
-  }, [url]);
+  }, [url, body]);
   return { data, loading, error };
 };
